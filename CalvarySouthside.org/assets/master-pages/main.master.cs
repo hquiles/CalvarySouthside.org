8a71d5683c820720e3bac71467e11d72d3a714a5
Ben Harrison <bharrison912@gmail.com>
2013-01-17 21:06:33 -0500

    continued progress
    google analytics, html5shim, jquery 1.9.0

--- CalvarySouthside.org/assets/master-pages/main.master.cs
+++ CalvarySouthside.org/assets/master-pages/main.master.cs
@@ -9,7 +9,18 @@ public partial class assets_Main : System.Web.UI.MasterPage
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        DisplayGoogleAnalytics();
+
         if (!IsPostBack)
+        {
             Page.Title = string.Format("Calvary Chapel Southside - {0}", Page.Title);
+        }
+    }
+
+    private void DisplayGoogleAnalytics()
+    {
+        // don't load google analytics in development environments
+        string hostName = Request.ServerVariables["SERVER_NAME"].ToLower();
+        pnlGoogleAnalytics.Visible = hostName.Contains("calvarysouthside");
     }
 }
