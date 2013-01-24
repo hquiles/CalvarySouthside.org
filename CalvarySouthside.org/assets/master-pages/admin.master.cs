b8e48679ee8dc49cb7288283bfec078078ee562f
Ben Harrison <bharrison912@gmail.com>
2013-01-23 23:14:13 -0500

    check for authenticated session

--- CalvarySouthside.org/assets/master-pages/admin.master.cs
+++ CalvarySouthside.org/assets/master-pages/admin.master.cs
@@ -4,11 +4,22 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Infrastructure;
 
 public partial class assets_master_pages_admin : System.Web.UI.MasterPage
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        // session id
+        // name
 
+        if (Authentication.IsAuthenticated)
+        {
+            hlnkUsername.Text = Session["Name"].ToString();
+        }
+        else
+        {
+            Response.Redirect("~/admin");
+        }
     }
 }
