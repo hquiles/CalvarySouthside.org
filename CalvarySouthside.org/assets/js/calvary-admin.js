bf2add7a70d4d6e37cae6610cb4cff1ff2de385e
Ben Harrison <bharrison912@gmail.com>
2013-02-05 22:36:34 -0500

    global javascript

---
+++ CalvarySouthside.org/assets/js/calvary-admin.js
@@ -0,0 +1,15 @@
+﻿(function($){
+    $(document).ready(function(){
+        // set 'active' class on the current menu item
+        var pathname = window.location.pathname.toLowerCase();
+        var page = pathname;
+        if (pathname.lastIndexOf('/') >= 0)
+            page = pathname.substring(pathname.lastIndexOf('/') + 1);
+            
+        $('ul.nav').find('a').each(function(index, obj){
+            var url = $(obj).attr('href');
+            if ( url.indexOf(page) >= 0 )
+                $(obj).parent('li').addClass('active');
+        });
+    });
+})(jQuery);
