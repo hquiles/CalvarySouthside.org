d7ec61291722e663c336ded406662485fcdd9546
Ben Harrison <bharrison912@gmail.com>
2014-01-31 15:00:16 -0800

    changes. its just the way it is.

---
+++ Core/Communication/EmailCore.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Collections.Specialized;
+using System.Net;
+using System.Net.Mail;
+
+namespace Core.Communication
+{
+    internal static class EmailCore
+    {
+        internal static void SendEmail(string to, string from, string cc, string subject, string message)
+        {
+            NameValueCollection emailSettings = ConfigurationManager.GetSection("EmailSettings") as NameValueCollection;
+
+            foreach (var key in emailSettings)
+                Console.WriteLine("{0} {1}", key, emailSettings[key.ToString()]);
+
+            MailMessage mail = new MailMessage();
+            mail.To.Add(new MailAddress(to));
+            mail.From = new MailAddress(from);
+            mail.Subject = subject;
+            mail.Body = message;
+
+            SmtpClient client = new SmtpClient();
+            client.UseDefaultCredentials = false;
+            client.Credentials = new NetworkCredential(emailSettings["smtpUsername"], emailSettings["smtpPassword"]);
+            client.Host = emailSettings["smtpServer"];
+            client.Port = int.Parse(emailSettings["smtpPort"]);
+            client.EnableSsl = bool.Parse(emailSettings["smtpEnableSsl"]);
+
+            client.Send(mail);
+        }
+    }
+}
