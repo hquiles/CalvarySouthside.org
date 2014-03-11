75dfb5080972274b63c23bdc418ded656a6b32bf
Ben Harrison <bharrison912@gmail.com>
2014-03-10 23:54:30 -0400

    responsive homepage

---
+++ CalvarySouthside/ConfigurationHelper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace CalvarySouthside
+{
+    public static class ConfigurationHelper
+    {
+        public static string ChurchAddress
+        {
+            get { return ConfigurationManager.AppSettings["ChurchAddress"]; }
+        }
+
+        public static string ChurchPhoneNumber
+        {
+            get { return ConfigurationManager.AppSettings["ChurchPhoneNumber"]; }
+        }
+
+        public static string ChurchEmailAddress
+        {
+            get { return ConfigurationManager.AppSettings["ChurchEmailAddress"]; }
+        }
+
+        public static string GoogleMapsUrl
+        {
+            get { return ConfigurationManager.AppSettings["GoogleMapsUrl"]; }
+        }
+
+    }
+}
