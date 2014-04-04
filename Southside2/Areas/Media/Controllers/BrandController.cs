7b4439eb37dfaf2708cade8f1d4719d4ffd7c620
Ben Harrison <bharrison912@gmail.com>
2014-04-03 22:43:09 -0400

    - created media area - created initial brand kit page - section in master layout for page specific css

---
+++ Southside2/Areas/Media/Controllers/BrandController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Southside2.Areas.Media.Controllers
+{
+    public class BrandController : Controller
+    {
+        //
+        // GET: /Media/Brand/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
